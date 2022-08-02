f53832bbcf57c5de2878097d0198ce9b06bcbfe9
Kurt Hutten IrevDev <k.hutten@protonmail.ch>
2022-08-02 12:14:58 +1000

    setup workflow

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,6 +1,6 @@
 import { file } from '../src/index.js';
+import type { Models } from '../src/index.js';
 import fsp from 'fs/promises';
-import { CodeOutput_type } from '../src/models.js';
 
 describe('Testing create_file_execution', () => {
   it("shouldn't throw", async () => {
@@ -8,7 +8,7 @@ describe('Testing create_file_execution', () => {
       lang: 'go',
       output: 'output.stl',
       body: await fsp.readFile('./exampleGoScript.go', 'base64'),
-    })) as CodeOutput_type;
+    })) as Models['CodeOutput_type'];
     expect(stderr).toBe('');
     expect(stdout).toBe(
       [
