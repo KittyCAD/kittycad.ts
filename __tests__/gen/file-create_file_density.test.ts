cc799cabe7011f2ca701e99822a0c7a2cb77308a
Kurt Hutten IrevDev <k.hutten@protonmail.ch>
2022-08-09 20:21:03 +1000

    add body to file gen tests

--- __tests__/gen/file-create_file_density.test.ts
+++ __tests__/gen/file-create_file_density.test.ts
@@ -1,7 +1,7 @@
 import { file } from '../../src/index.js';
 
 async function example() {
-  const response = await file.create_file_density({ material_mass: 7, src_format: 'FileSourceFormat_type' });
+  const response = await file.create_file_density({ material_mass: 7, src_format: 'stl', body: 'base64 encoded string' });
   if ('error_code' in response) throw 'error';
   console.log(JSON.stringify(response, null, 2));
   return response;
--- __tests__/gen/file-create_file_execution.test.ts
+++ __tests__/gen/file-create_file_execution.test.ts
@@ -1,7 +1,7 @@
 import { file } from '../../src/index.js';
 
 async function example() {
-  const response = await file.create_file_execution({ lang: 'CodeLanguage_type', output: 'string' });
+  const response = await file.create_file_execution({ lang: 'go', output: 'string', body: 'base64 encoded string' });
   if ('error_code' in response) throw 'error';
   console.log(JSON.stringify(response, null, 2));
   return response;
