4e0b728382027298874096a920e949e5ae002026
Jess Frazelle <jessfraz@users.noreply.github.com>
2023-10-17 15:36:14 -0700

    Update api spec (#192)
    * YOYO NEW API SPEC!
    
    * Generated new lib
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ __tests__/gen/ai-list_text_to_cad_models_for_user.test.ts
@@ -0,0 +1,18 @@
+import { ai } from '../../src/index.js';
+
+async function example() {
+  const response = await ai.list_text_to_cad_models_for_user({
+    limit: 7,
+    page_token: 'string',
+    sort_by: 'created_at_ascending',
+  });
+  if ('error_code' in response) throw response;
+
+  return response;
+}
+
+describe('Testing ai.list_text_to_cad_models_for_user', () => {
+  it('should be truthy or throw', async () => {
+    expect(await example()).toBeTruthy();
+  });
+});
---
+++ src/api/ai/list_text_to_cad_models_for_user.ts
@@ -0,0 +1,43 @@
+import fetch from 'node-fetch';
+import {
+  TextToCadResultsPage_type,
+  Error_type,
+  CreatedAtSortMode_type,
+} from '../../models.js';
+import { Client } from '../../client.js';
+
+interface List_text_to_cad_models_for_user_params {
+  client?: Client;
+  limit: number;
+  page_token: string;
+  sort_by: CreatedAtSortMode_type;
+}
+
+type List_text_to_cad_models_for_user_return =
+  | TextToCadResultsPage_type
+  | Error_type;
+
+export default async function list_text_to_cad_models_for_user({
+  client,
+  limit,
+  page_token,
+  sort_by,
+}: List_text_to_cad_models_for_user_params): Promise<List_text_to_cad_models_for_user_return> {
+  const url = `/user/text-to-cad?limit=${limit}&page_token=${page_token}&sort_by=${sort_by}`;
+  const urlBase = process?.env?.BASE_URL || 'https://api.kittycad.io';
+  const fullUrl = urlBase + url;
+  const kittycadToken = client
+    ? client.token
+    : process.env.KITTYCAD_TOKEN || '';
+  const headers = {
+    Authorization: `Bearer ${kittycadToken}`,
+  };
+  const fetchOptions = {
+    method: 'GET',
+    headers,
+  };
+  const response = await fetch(fullUrl, fetchOptions);
+  const result =
+    (await response.json()) as List_text_to_cad_models_for_user_return;
+  return result;
+}
