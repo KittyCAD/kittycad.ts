501c98f36fbf9860cad364ea69973fef79eed165
Max Ammann <max.ammann@zoo.dev>
2025-06-02 15:37:36 +0200

    Revert

--- __tests__/gen/users-patch_user_crm.test.ts
+++ __tests__/gen/users-patch_user_crm.test.ts
@@ -4,8 +4,8 @@ async function example() {
   const response = await users.patch_user_crm({
     body: {
       cad_industry: 'The industry of the user.',
+      cad_user_count: 'The user count of the user.',
       cad_user_type: 'The user type.',
-      number_of_cad_users: 'The user count of the user.',
     },
   });
   if ('error_code' in response) throw response;
@@ -15,10 +15,6 @@ async function example() {
 
 describe('Testing users.patch_user_crm', () => {
   it('should be truthy or throw', async () => {
-    try {
-      await example();
-    } catch (err) {
-      expect(err).toBeTruthy(); // eslint-disable-line jest/no-conditional-expect
-    }
+    expect(await example()).toBeTruthy();
   });
 });
