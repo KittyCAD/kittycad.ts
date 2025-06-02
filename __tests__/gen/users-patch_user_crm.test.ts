e591d269d5c02e6682be000577152570b92342a0
Max Ammann <max.ammann@zoo.dev>
2025-06-02 15:40:29 +0200

    update

--- __tests__/gen/users-patch_user_crm.test.ts
+++ __tests__/gen/users-patch_user_crm.test.ts
@@ -4,8 +4,8 @@ async function example() {
   const response = await users.patch_user_crm({
     body: {
       cad_industry: 'The industry of the user.',
-      cad_user_count: 'The user count of the user.',
       cad_user_type: 'The user type.',
+      number_of_cad_users: 'The user count of the user.',
     },
   });
   if ('error_code' in response) throw response;
