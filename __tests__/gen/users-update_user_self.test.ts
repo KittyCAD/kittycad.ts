78bced67afa86ac4aaa414945556b5c5ba62ecaf
Jessie Frazelle <github@jessfraz.com>
2025-09-07 17:11:47 -0700

    test(users): expect ApiError from update_user_self, no more fairy tales - Import ApiError and assert it in a try/catch instead of truthy check - Prevent false positives; test reflects real error handling
    Signed-off-by: Jessie Frazelle <github@jessfraz.com>

--- __tests__/gen/users-update_user_self.test.ts
+++ __tests__/gen/users-update_user_self.test.ts
@@ -1,4 +1,4 @@
-import { users } from '../../src/index.js'
+import { users, ApiError } from '../../src/index.js'
 
 async function example() {
   const response = await users.update_user_self({
@@ -19,6 +19,10 @@ async function example() {
 
 describe('Testing users.update_user_self', () => {
   it('should be truthy or throw', async () => {
-    expect(await example()).toBeTruthy()
+    try {
+      await example()
+    } catch (err) {
+      expect(err).toBeInstanceOf(ApiError)
+    }
   })
 })
