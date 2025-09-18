51c3e6cf93cd7807cda4a2e74e6fd3b69838962c
Paul Tagliamonte <paultag@gmail.com>
2025-09-18 14:49:04 -0400

    fix more paths

--- __tests__/gen/ml-create_text_to_cad_part_feedback.test.ts
+++ __tests__/gen/ml-create_text_to_cad_part_feedback.test.ts
@@ -1,11 +1,14 @@
 import { ml, ApiError } from '../../src/index.js'
 
 async function example() {
-  const response = await ml.get_text_to_cad_model_for_user({ id: 'string' })
+  const response = await ml.create_text_to_cad_part_feedback({
+    id: 'string',
+    feedback: 'thumbs_up',
+  })
   return response
 }
 
-describe('Testing ml.get_text_to_cad_model_for_user', () => {
+describe('Testing ml.create_text_to_cad_part_feedback', () => {
   it('should be truthy or throw', async () => {
     try {
       await example()
--- __tests__/gen/ml-get_text_to_cad_part_for_user.test.ts
+++ __tests__/gen/ml-get_text_to_cad_part_for_user.test.ts
@@ -1,14 +1,11 @@
 import { ml, ApiError } from '../../src/index.js'
 
 async function example() {
-  const response = await ml.create_text_to_cad_model_feedback({
-    id: 'string',
-    feedback: 'thumbs_up',
-  })
+  const response = await ml.get_text_to_cad_part_for_user({ id: 'string' })
   return response
 }
 
-describe('Testing ml.create_text_to_cad_model_feedback', () => {
+describe('Testing ml.get_text_to_cad_part_for_user', () => {
   it('should be truthy or throw', async () => {
     try {
       await example()
