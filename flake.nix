742cba0300c609619aec4be57be774d2c39e402d
Jessie Frazelle <github@jessfraz.com>
2025-11-10 14:28:56 -0800

    updates
    Signed-off-by: Jessie Frazelle <github@jessfraz.com>

--- flake.nix
+++ flake.nix
@@ -7,38 +7,41 @@
   };
 
   # Flake outputs
-  outputs = { self, nixpkgs }:
-    let
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # Systems supported
+    allSystems = [
+      "x86_64-linux" # 64-bit Intel/AMD Linux
+      "aarch64-linux" # 64-bit ARM Linux
+      "x86_64-darwin" # 64-bit Intel macOS
+      "aarch64-darwin" # 64-bit ARM macOS
+    ];
 
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-
-    in
-    {
-      # Development environment output
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
-          # The Nix packages provided in the environment
-          packages = (with pkgs; [
+    # Helper to provide system-specific attributes
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    # Development environment output
+    devShells = forAllSystems ({pkgs}: {
+      default = pkgs.mkShell {
+        # The Nix packages provided in the environment
+        packages =
+          (with pkgs; [
             just
             nodejs_22
-            npm
-          ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
-            libiconv 
+          ])
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
+            libiconv
             darwin.apple_sdk.frameworks.Security
           ]);
 
-          TARGET_CC = "${pkgs.stdenv.cc}/bin/${pkgs.stdenv.cc.targetPrefix}cc";
-        };
-      });
-    };
+        TARGET_CC = "${pkgs.stdenv.cc}/bin/${pkgs.stdenv.cc.targetPrefix}cc";
+      };
+    });
+  };
 }
