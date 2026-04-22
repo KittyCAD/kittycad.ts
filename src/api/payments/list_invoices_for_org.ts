b76db60a49feea1c6924d4bc8a8312d9431bdfa6
Max Ammann <max.ammann@zoo.dev>
2026-04-22 11:55:55 +0200

    generate tests

--- src/api/payments/list_invoices_for_org.ts
+++ src/api/payments/list_invoices_for_org.ts
@@ -1,13 +1,16 @@
 import { Client, buildQuery } from '../../client.js'
 import { throwIfNotOk } from '../../errors.js'
+import { Pager, createPager } from '../../pagination.js'
 
-import { Invoice } from '../../models.js'
+import { InvoiceResultsPage, Invoice } from '../../models.js'
 
 interface ListInvoicesForOrgInput {
   client?: Client
+  limit?: number
+  page_token?: string
 }
 
-type ListInvoicesForOrgReturn = Invoice[]
+type ListInvoicesForOrgReturn = InvoiceResultsPage
 
 /**
  * List invoices for your org.
@@ -18,15 +21,19 @@ type ListInvoicesForOrgReturn = Invoice[]
  *
  * @param params Function parameters.
  * @property {Client} [client] Optional client with auth token.
+ * @property {number} limit Maximum number of items returned by a single call (query)
+ * @property {string} page_token Token returned by previous call to retrieve the subsequent page (query)
  * @returns {Promise<ListInvoicesForOrgReturn>} successful operation
  *
- * Possible return types: Invoice[]
+ * Possible return types: InvoiceResultsPage
  */
-export default async function list_invoices_for_org(
-  { client }: ListInvoicesForOrgInput = {} as ListInvoicesForOrgInput
-): Promise<ListInvoicesForOrgReturn> {
+export default async function list_invoices_for_org({
+  client,
+  limit,
+  page_token,
+}: ListInvoicesForOrgInput): Promise<ListInvoicesForOrgReturn> {
   const path = `/org/payment/invoices`
-  const qs = buildQuery({})
+  const qs = buildQuery({ limit: limit, page_token: page_token })
   const url = path + qs
   // Backwards compatible for the BASE_URL env variable
   // That used to exist in only this lib, ZOO_HOST exists in the all the other
@@ -45,4 +52,14 @@ export default async function list_invoices_for_org(
   await throwIfNotOk(response)
   const result = (await response.json()) as ListInvoicesForOrgReturn
   return result
+}
+
+export function list_invoices_for_org_pager(
+  params: ListInvoicesForOrgInput
+): Pager<ListInvoicesForOrgInput, ListInvoicesForOrgReturn, Invoice> {
+  return createPager<
+    ListInvoicesForOrgInput,
+    ListInvoicesForOrgReturn,
+    Invoice
+  >(list_invoices_for_org, params, 'page_token')
 }
--- src/api/payments/list_invoices_for_user.ts
+++ src/api/payments/list_invoices_for_user.ts
@@ -1,13 +1,16 @@
 import { Client, buildQuery } from '../../client.js'
 import { throwIfNotOk } from '../../errors.js'
+import { Pager, createPager } from '../../pagination.js'
 
-import { Invoice } from '../../models.js'
+import { InvoiceResultsPage, Invoice } from '../../models.js'
 
 interface ListInvoicesForUserInput {
   client?: Client
+  limit?: number
+  page_token?: string
 }
 
-type ListInvoicesForUserReturn = Invoice[]
+type ListInvoicesForUserReturn = InvoiceResultsPage
 
 /**
  * List invoices for your user.
@@ -18,15 +21,19 @@ type ListInvoicesForUserReturn = Invoice[]
  *
  * @param params Function parameters.
  * @property {Client} [client] Optional client with auth token.
+ * @property {number} limit Maximum number of items returned by a single call (query)
+ * @property {string} page_token Token returned by previous call to retrieve the subsequent page (query)
  * @returns {Promise<ListInvoicesForUserReturn>} successful operation
  *
- * Possible return types: Invoice[]
+ * Possible return types: InvoiceResultsPage
  */
-export default async function list_invoices_for_user(
-  { client }: ListInvoicesForUserInput = {} as ListInvoicesForUserInput
-): Promise<ListInvoicesForUserReturn> {
+export default async function list_invoices_for_user({
+  client,
+  limit,
+  page_token,
+}: ListInvoicesForUserInput): Promise<ListInvoicesForUserReturn> {
   const path = `/user/payment/invoices`
-  const qs = buildQuery({})
+  const qs = buildQuery({ limit: limit, page_token: page_token })
   const url = path + qs
   // Backwards compatible for the BASE_URL env variable
   // That used to exist in only this lib, ZOO_HOST exists in the all the other
@@ -45,4 +52,14 @@ export default async function list_invoices_for_user(
   await throwIfNotOk(response)
   const result = (await response.json()) as ListInvoicesForUserReturn
   return result
+}
+
+export function list_invoices_for_user_pager(
+  params: ListInvoicesForUserInput
+): Pager<ListInvoicesForUserInput, ListInvoicesForUserReturn, Invoice> {
+  return createPager<
+    ListInvoicesForUserInput,
+    ListInvoicesForUserReturn,
+    Invoice
+  >(list_invoices_for_user, params, 'page_token')
 }
